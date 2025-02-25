ddbc817a3f7edf44b9ad8fd5e15b5e6c8b67109a
danimelenge <danielmelenge@gmail.com>
2025-02-25 13:55:39 -0500

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Daniel Melenge Rojas on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
