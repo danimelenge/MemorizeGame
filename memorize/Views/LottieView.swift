1c8d14d008fc3a5f54b54a05e345a9872357d4fe
danimelenge <danielmelenge@gmail.com>
2025-11-25 15:19:27 -0500

    Add Lottie 4.5.2 package and LottieView for animations

--- memorize/Views/LottieView.swift
+++ memorize/Views/LottieView.swift
@@ -5,4 +5,41 @@
 //  Created by Daniel Melenge Rojas on 25/11/25.
 //
 
-import Foundation
+import SwiftUI
+import Lottie
+
+/// UIViewRepresentable wrapper para Lottie en SwiftUI
+struct LottieView: UIViewRepresentable {
+    let name: String
+    var loopMode: LottieLoopMode = .playOnce
+    var contentMode: UIView.ContentMode = .scaleAspectFit
+
+    func makeUIView(context: Context) -> some UIView {
+        let containerView = UIView(frame: .zero)
+
+        let animation = LottieAnimation.named(name)
+        let animationView = LottieAnimationView(animation: animation)
+        animationView.contentMode = contentMode
+        animationView.loopMode = loopMode
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+
+        containerView.addSubview(animationView)
+
+        NSLayoutConstraint.activate([
+            animationView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            animationView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            animationView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            animationView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+        ])
+
+        animationView.play { _ in
+            // opcional: acciones cuando termina
+        }
+
+        return containerView
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        // No-op
+    }
+}
